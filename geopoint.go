a8917770c63e7fa266f6f1abab075cb946ef992b
Nicola Ferruzzi <nicola.ferruzzi@gmail.com>
2015-06-22 20:00:19 +0200

    Geopoint

---
+++ geopoint.go
@@ -0,0 +1,55 @@
+package gormGIS
+
+import (
+	"bytes"
+	"database/sql/driver"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+)
+
+type GeoPoint struct {
+	Lng float64
+	Lat float64
+}
+
+func (p *GeoPoint) String() string {
+	return fmt.Sprintf("SRID=4326;POINT(%v %v)", p.Lng, p.Lat)
+}
+
+func (p *GeoPoint) Scan(val interface{}) error {
+	b, err := hex.DecodeString(string(val.([]uint8)))
+	if err != nil {
+		return err
+	}
+	r := bytes.NewReader(b)
+	var wkbByteOrder uint8
+	if err := binary.Read(r, binary.LittleEndian, &wkbByteOrder); err != nil {
+		return err
+	}
+
+	var byteOrder binary.ByteOrder
+	switch wkbByteOrder {
+	case 0:
+		byteOrder = binary.BigEndian
+	case 1:
+		byteOrder = binary.LittleEndian
+	default:
+		return fmt.Errorf("Invalid byte order %d", wkbByteOrder)
+	}
+
+	var wkbGeometryType uint64
+	if err := binary.Read(r, byteOrder, &wkbGeometryType); err != nil {
+		return err
+	}
+
+	if err := binary.Read(r, byteOrder, p); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (p GeoPoint) Value() (driver.Value, error) {
+	return p.String(), nil
+}
